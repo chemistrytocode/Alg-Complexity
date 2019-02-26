663148858fdf2521c8dd028487905b079fc3f663
isabelcooper <isabel@triptease.com>
2019-02-26 15:38:43 +0000

    2 lines on one chart

--- interface.js
+++ interface.js
@@ -1,22 +1,29 @@
 // $(document).ready(function() {
 // })
+var datasets = [];
+var data = [];
+var index = 0;
 
 function returnId(id) {
-  var type = id
-  generateResults()
+  var type = id;
+  generateResults();
 
   function generateResults() {
-    var data = [];
-    $("#colName").text(type)
-    populateDataArray()
+    $("#colName").text(type);
+    populateDataArray();
 
     function populateDataArray() {
       var numberOfIterations = 1;
-      var colIdentifier = type
+      var colIdentifier = type;
+      var colors = [
+        "rgba(83, 51, 237, 1)",
+        "rgba(219, 10, 91, 1)",
+        "rgba(38, 194, 129, 1)"
+      ];
 
       while (numberOfIterations <= 20) {
         var time = timeTesting(
-          numberOfIterations * 50000,
+          numberOfIterations * 500,
           eval(colIdentifier),
           100
         );
@@ -24,7 +31,29 @@ function returnId(id) {
         data.push(time);
         numberOfIterations++;
       }
+      dataset = createDataset();
+      datasets.push(dataset);
+      reset();
+
+      function reset() {
+        index === colors.length-1 ? (index = 0) : index++;
+        data = [];
+      }
+
+      function randomColor() {
+        return colors[index];
+      }
+
+      function createDataset() {
+        return {
+          label: type,
+          borderColor: randomColor(),
+          fill: false,
+          data: data
+        };
+      }
     }
+  }
 
   var ctx = document.getElementById("myChart").getContext("2d");
   var chart = new Chart(ctx, {
@@ -52,16 +81,8 @@ function returnId(id) {
         "95000",
         "100000"
       ],
-      datasets: [
-        {
-          label: type,
-          borderColor: "rgb(255, 99, 132)",
-          data: data
-        }
-      ]
+      datasets: datasets
     },
     options: {}
   });
-  }
-
 }
