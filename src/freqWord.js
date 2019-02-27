61aa7a41d43be50880963ee7a06640c80585d2f1
isabelcooper <isabel@triptease.com>
2019-02-27 14:44:56 +0000

    freqword

--- src/freqWord.js
+++ src/freqWord.js
@@ -1,27 +1,23 @@
 function freqWord(array) {
-  // return array
-  //      .reduce((acc, cur, ind, arr) => {
-  //        if (arr.indexOf(cur) === ind) {
-  //          return [...acc, [cur, 1]];
-  //        } else {
-  //          acc[acc.indexOf(acc.find(e => e[0] === cur))] = [
-  //            cur,
-  //            acc[acc.indexOf(acc.find(e => e[0] === cur))][1] + 1
-  //          ];
-  //          return acc;
-  //        }
-  //      }, [])
-  //      .sort((a, b) => b[1] - a[1])
-  //      .filter((cur, ind, arr) => cur[1] === arr[0][1])
-  //      .map(cur => cur[0]);
-  //  }
-
+  return array.reduce((acc, cur, ind, arr) => {
+      if (arr.indexOf(cur) === ind) {
+        return [...acc, [cur, 1]];
+      } else {
+        acc[acc.indexOf(acc.find(e => e[0] === cur))] = [
+          cur,
+          acc[acc.indexOf(acc.find(e => e[0] === cur))][1] + 1
+        ];
+        return acc;
+      }
+    }, [])
+    .sort((a, b) => b[1] - a[1])
+    .filter((cur, ind, arr) => cur[1] === arr[0][1])
+    .map(cur => cur[0])[0];
 }
 
-
-  //
-  // var hash = [];
-  // for (var i = 0; i < array.length; i++) {
-  //   if ( hash[array[i]]) {hash[array[i]] += 1}
-  //   else { hash[array[i]] = 1 }
-  // }
+//
+// var hash = [];
+// for (var i = 0; i < array.length; i++) {
+//   if ( hash[array[i]]) {hash[array[i]] += 1}
+//   else { hash[array[i]] = 1 }
+// }
