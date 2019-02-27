1b3d28bbb705e23da6e05e2405730205eca00f0f
Paul <paul.ayling@bglgroup.co.uk>
2019-02-27 11:35:52 +0000

    Readme and conflict

--- src/reverse.js
+++ src/reverse.js
@@ -1,3 +1,14 @@
 function reverse(array) {
   return array.reverse();
 }
+
+
+function newReverse(array) {
+  const newArray = [];
+  const len = array.length;
+  for (i = 0; i < len; i++) {
+      item = array.pop()
+      newArray.push(item)
+  }
+  return newArray
+}
--- src/sort.js
+++ src/sort.js
@@ -3,19 +3,36 @@ function sort(array) {
   return array.sort();
 }
 
-// quicksort:
-function quickSort(arr) {
-  quicksort(arr);
+// // quicksort:
+// function quickSort(arr) {
+//   quicksort(arr);
+//
+//   function quicksort(arr, left = 0, right = arr.length - 1) {
+//     var len = arr.length,
+//       pivot,
+//       partitionIndex;
+//
+//     if (left < right) {
+//       pivot = right;
+//       partitionIndex = partition(arr, pivot, left, right);
+//
+//       //sort left and right
+//       quicksort(arr, left, partitionIndex - 1);
+//       quicksort(arr, partitionIndex + 1, right);
+//     }
+//   }
+// return arr;
+// }
 
-  function quicksort(arr, left = 0, right = arr.length - 1) {
-    var len = arr.length,
-      pivot,
-      partitionIndex;
+function defaultComparator(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  } else return 0;
+}
 
-    if (left < right) {
-      pivot = right;
-      partitionIndex = partition(arr, pivot, left, right);
-
+<<<<<<< HEAD
       //sort left and right
       quicksort(arr, left, partitionIndex - 1);
       quickssorts(arr, partitionIndex + 1, right);
@@ -25,23 +42,36 @@ function quickSort(arr) {
   function partistion(arr, pivot, left, right) {
     var pivotValue = arr[pivot],
       partitionIndexs = left;
+=======
+function quickSort(array, comparator = defaultComparator) {
+  recursiveSort(0, array.length - 1);
+  return array;
 
-    for (var i = left; i < right; i++) {
-      if (arr[i] < pivotValue) {
-        swap(arr, i, partitionIndex);
-        partitionIndex++;
+  function recursiveSort(start, end) {
+    const pivotValue = array[end];
+    let splitIndex = start;
+>>>>>>> 062cc51e97a13862373fb3ad5e632101bf1fd274
+
+    if (end - start < 1) {
+      return;
+    }
+    for (let i = start; i < end; i++) {
+      const sort = comparator(array[i], pivotValue);
+      if (sort === -1) {
+        if (splitIndex !== i) {
+          const temp = array[splitIndex];
+          array[splitIndex] = array[i];
+          array[i] = temp;
+        }
+        splitIndex++;
       }
     }
-    swap(arr, right, partitionIndex);
-    return partitionIndex;
-  }
+    array[end] = array[splitIndex];
+    array[splitIndex] = pivotValue;
 
-  function swap(arr, i, j) {
-    var temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+    recursiveSort(start, splitIndex - 1);
+    recursiveSort(splitIndex + 1, end);
   }
-  return arr;
 }
 
 //BubbleSort
